9e8e90eb6f2d4706ef16a012c665b0d67bf8f817
Kisleto <zsviktor45@gmail.com>
2019-03-25 13:53:56 +0100

    Backend architecture set up

---
+++ Backend/shitwishShop/src/main/java/com/codecool/shitwish/ShitwishApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.shitwish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShitwishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShitwishApplication.class, args);
+    }
+
+}
