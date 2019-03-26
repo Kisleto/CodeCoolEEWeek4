a68cf410fa1b0377080c2ff64f7ad3386bd040a7
Kisleto <zsviktor45@gmail.com>
2019-03-26 19:19:18 +0100

    cross origin added to controller

--- Backend/shitwishShop/src/main/java/com/codecool/shitwish/controller/ShopItemController.java
+++ Backend/shitwishShop/src/main/java/com/codecool/shitwish/controller/ShopItemController.java
@@ -4,10 +4,12 @@ import com.codecool.shitwish.entity.ShopItem;
 import com.codecool.shitwish.service.ShopItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/products")
 public class ShopItemController {
