b46253917e7168e9e9813bc0cd58f7b992db2e05
Kisleto <zsviktor45@gmail.com>
2019-03-26 16:31:26 +0100

    started working on items and their representaion.

--- frontend/src/components/item.js
+++ frontend/src/components/item.js
@@ -1,17 +1,37 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+
 
 class Item extends Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            items: []
+        }
+    }
+
+    componentDidMount(event) {
+        let shitWishAPIUrl = `http://localhost:8091/products/all`;
+        event.preventDefault();
+        axios.get(shitWishAPIUrl)
+            .then(res => {
+                const items = res.data;
+                this.setState({items});
+
+            });
     }
 
 
     render() {
-        return (
+        this.state.items.forEach(item => {
+            return (
             <div>
+                <div>{item.name}</div>
 
             </div>
-        )
+            )
+        })
+
     }
 
 }
