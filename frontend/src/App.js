e1a85e738e57407ffcd54ced7533d456cadd58f7
Kisleto <zsviktor45@gmail.com>
2019-03-26 16:02:18 +0100

    item components started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Header from "./components/header";
 
 class App extends Component {
   render() {
     return (
      <div>
-       <p>React</p>
+         <Header/>
      </div>
     );
   }
