73acdfa18eb4aa4e862fa93e58a155a80672397c
peterSzalma <petiszalma@gmail.com>
2019-03-27 14:56:15 +0100

    add customer data to db

--- Backend/login/src/main/java/com/codecool/login/controller/CustomerController.java
+++ Backend/login/src/main/java/com/codecool/login/controller/CustomerController.java
@@ -1,4 +1,31 @@
 package com.codecool.login.controller;
 
+import com.codecool.login.entity.Customer;
+import com.codecool.login.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @PostMapping
+    public void saveCustomersData(@RequestParam String name, @RequestParam String email, @RequestParam String password) {
+        Customer customer = Customer.builder()
+                .name(name)
+                .email(email)
+                .password(password)
+                .build();
+
+        customerRepository.save(customer);
+    }
+
+    @GetMapping
+    public List<Customer> getAllCustomer() {
+        return customerRepository.findAll();
+    }
 }
--- Backend/login/src/main/java/com/codecool/login/service/CustomerService.java
+++ Backend/login/src/main/java/com/codecool/login/service/CustomerService.java
@@ -1,4 +1,16 @@
 package com.codecool.login.service;
 
+import com.codecool.login.entity.Customer;
+import com.codecool.login.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public Customer getOneCustomer(long id) {
+        return customerRepository.getOne(id);
+    }
 }
